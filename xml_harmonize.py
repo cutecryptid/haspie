a6620adda5872913af28041a9c44f958d146e238
Trigork <trigork@gmail.com>
2015-10-25 21:54:34 +0100

    Added minor and major modes to ASP processing module and as an option to the python pipeline. Included output parsing for later processing

--- xml_harmonize.py
+++ xml_harmonize.py
@@ -2,22 +2,59 @@ import argparse
 import subprocess
 import re
 
-parser = argparse.ArgumentParser(description='Harmonizing music with ASP')
-parser.add_argument('xml_score', metavar='XML_SCORE',
-                   help='input musicXML score for armonizing')
-parser.add_argument('-n', metavar='N', nargs=1, default=1, type=int,
-                   help='max number of ASP solutions')
+def parse_out(asp_out):
+	satis = re.search('SATISFIABLE', asp_out)
+	solution = []
+	if satis == None:
+		print "UNSATISFIABLE"
+		return solution
+	solutions = re.compile("Answer:\s*[0-9]+").split(asp_out)
 
-args = parser.parse_args()
+	for sol in solutions:
+		chord = re.findall('chord\([0-9]+,[0-9]+\)', sol)
+		if len(sol) > 0:
+			solution += [sorted(chord)]
+	return solution
 
-infile = args.xml_score
-n = args.n[0]
-asp_outfile_name = re.search('/(.*?)\.xml', infile)
-outname = asp_outfile_name.group(1)
-lp_outname = outname + ".lp"
-xml_parser_args = ("parser/mxml_asp", infile, "-o", "asp/generated_logic_music/" + lp_outname)
-xml_parser = subprocess.call(xml_parser_args)
+def main():
+	parser = argparse.ArgumentParser(description='Harmonizing music with ASP')
+	parser.add_argument('xml_score', metavar='XML_SCORE',
+	                   help='input musicXML score for armonizing')
+	parser.add_argument('-n', metavar='N', nargs=1, default=1, type=int,
+	                   help='max number of ASP solutions')
+	parser.add_argument('-m', metavar='[major|minor]', nargs=1, default="major",
+	                   help='mode')
 
-# TODO install clingo wrapper, by now we'll be using calls
-asp_args = ("clingo", "asp/assign_chords_black.lp", "asp/generated_logic_music/" + lp_outname, "-n", str(n))
-asp = subprocess.call(asp_args)
+	args = parser.parse_args()
+
+	infile = args.xml_score
+
+	n = args.n
+	if args.n != 1:
+		n = args.n[0]
+
+	mode = args.m
+	if args.m != "major":
+		mode = args.m[0]
+
+	asp_outfile_name = re.search('/(.*?)\.xml', infile)
+	outname = asp_outfile_name.group(1)
+	lp_outname = outname + ".lp"
+	xml_parser_args = ("parser/mxml_asp", infile, "-o", "asp/generated_logic_music/" + lp_outname)
+	xml_parser = subprocess.call(xml_parser_args)
+	asp_args = ("clingo", "asp/assign_chords_black.lp", "asp/include/" + mode + "_mode.lp" ,
+		"asp/generated_logic_music/" + lp_outname, "-n", str(n))
+	asp_proc = subprocess.Popen(asp_args, stdout=subprocess.PIPE)
+	asp_out = asp_proc.stdout.read()
+
+	solution = parse_out(asp_out)
+	if len(solution) > 0:
+		print "SATISFIABLE"
+		for sol in solution:
+			print sol
+	else:
+		print "UNSATISFIABLE"
+
+
+if __name__ == "__main__":
+    main()
