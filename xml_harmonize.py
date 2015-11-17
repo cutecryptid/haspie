67f40fbec21fdc947d528b9817dbec644c78b1b1
trigork <trigork>
2015-11-17 11:21:23 +0100

    Detected Unsatifiability and implemented timeout

--- xml_harmonize.py
+++ xml_harmonize.py
@@ -3,6 +3,8 @@ import subprocess
 import re
 import sys
 import os
+import threading
+import errno
 sys.path.append('./out_module') 
 import Out
 
@@ -98,28 +100,31 @@ class ClaspResult:
 		solutions = []
 		for ans in answers:
 			if len(ans) > 0:
-				notes = re.findall('out_note\(([0-9]+),([0-9]+),([0-9]+)\)', ans)
-				voices = {};
-				for note in notes:
-					if (int(note[0]) in voices.keys()):
-						voices[int(note[0])].append(Note(int(note[1]),int(note[2])))
-					else:
-						voices.update({(int(note[0])) : [Note(int(note[1]),int(note[2]))]})
-				rests = re.findall('rest\(([0-9]+),([0-9]+)\)', ans)
-				for rest in rests:
-					if (int(rest[0]) in voices.keys()):
-						voices[int(rest[0])].append(Rest(int(rest[1])))
-					else:
-						voices.update({(int(rest[0])) : [Rest(int(rest[1]))]})
+				try:
+					notes = re.findall('out_note\(([0-9]+),([0-9]+),([0-9]+)\)', ans)
+					voices = {};
+					for note in notes:
+						if (int(note[0]) in voices.keys()):
+							voices[int(note[0])].append(Note(int(note[1]),int(note[2])))
+						else:
+							voices.update({(int(note[0])) : [Note(int(note[1]),int(note[2]))]})
+					rests = re.findall('rest\(([0-9]+),([0-9]+)\)', ans)
+					for rest in rests:
+						if (int(rest[0]) in voices.keys()):
+							voices[int(rest[0])].append(Rest(int(rest[1])))
+						else:
+							voices.update({(int(rest[0])) : [Rest(int(rest[1]))]})
 
-				voices = {k: sorted(v, key=lambda tup: tup.time) for k, v in voices.items()}
+					voices = {k: sorted(v, key=lambda tup: tup.time) for k, v in voices.items()}
 
-				chords = [Chord(int(ch[0]),ch[1]) for ch in sorted(re.findall('chord\(([0-9]+),([ivxmo7]+)\)', ans))]
-				errors = [Error(int(er[0]),int(er[1]),int(er[2])) for er in re.findall('error\(([0-9]+),([0-9]+),([0-9]+)\)', ans)]
-				str_opts = re.split("\s*", re.search('Optimization:((?:\s*[0-9]+)+)', ans).group(1))
-				taw = str_opts.pop(0)
-				optimums = map(int, str_opts)
-				solutions += [HaspSolution(chords,voices,errors,optimums)]
+					chords = [Chord(int(ch[0]),ch[1]) for ch in sorted(re.findall('chord\(([0-9]+),([ivxmo7]+)\)', ans))]
+					errors = [Error(int(er[0]),int(er[1]),int(er[2])) for er in re.findall('error\(([0-9]+),([0-9]+),([0-9]+)\)', ans)]
+					str_opts = re.split("\s*", re.search('Optimization:((?:\s*[0-9]+)+)', ans).group(1))
+					taw = str_opts.pop(0)
+					optimums = map(int, str_opts)
+					solutions += [HaspSolution(chords,voices,errors,optimums)]
+				except AttributeError:
+					print "Discarding incomplete answer due to early temrination."
 		return solutions
 
 	def __str__(self):
@@ -131,6 +136,14 @@ class ClaspResult:
 			ansno += 1
 		return ret
 
+def clasp_timeout( p ):
+    if p.poll() is None:
+        try:
+            p.kill()
+            print 'Timeout reached, terminating clasp'
+        except OSError as e:
+            if e.errno != errno.ESRCH:
+                print 'All options explored before timeout'
 		
 def main():
 	parser = argparse.ArgumentParser(description='Harmonizing music with ASP')
@@ -214,16 +227,26 @@ def main():
 
 	asp_args = ("clingo", "asp/assign_chords.lp", "asp/include/" + mode + "or_mode.lp", "asp/include/" + mode + "or_chords.lp",
 		"asp/include/conversions.lp", "asp/generated_logic_music/" + lp_outname, "-n", str(n), "--const", "span=" + str(span), 
-		"--const", "extra_voices="+ str(voices), "--const", "base="+ str(base), opt_all)
+		"--const","extra_voices="+ str(voices), "--const", "base="+ str(base), opt_all)
 
 	asp_proc = subprocess.Popen(asp_args, stdout=subprocess.PIPE)
+
+	t = threading.Timer( timeout, clasp_timeout, [asp_proc] )
+	t.start()
+	t.join()
+    
 	asp_out = asp_proc.stdout.read()
+
+	t.cancel()
+
+	if (re.search("UNSATISFIABLE",asp_out) != None):
+		sys.exit("UNSATISFIABLE, stopping execution.")
 
 	res = ClaspResult(asp_out)
 	print res
 
 	sol_num = len(res.solutions)
-	selected_solution = raw_input('Select a solution to output (0..' + str(sol_num) +') [' + str(sol_num) + ']: ')
+	selected_solution = raw_input('Select a solution to output (1..' + str(sol_num) +') [' + str(sol_num) + ']: ')
 	if selected_solution == '':
 		selected_solution = sol_num
 	print res.solutions[int(selected_solution)-1]
