c06b555e13239aef1a2b90853e25166bd830606b
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2019-09-20 03:37:13 +0300

    Make options as list
    https://stackoverflow.com/questions/6800481/python-map-object-is-not-subscriptable

--- lib/HaspMusic.py
+++ lib/HaspMusic.py
@@ -276,7 +276,7 @@ class ClaspChords:
 			if len(ans) > 0:
 				str_opts = re.split("\s*", re.search('Optimization:((?:\s*[0-9]+)+)', ans).group(1))
 				str_opts = [a for a in str_opts if len(a) > 0]
-				tmp_opts = map(int, str_opts)
+				tmp_opts = list(map(int, str_opts))
 				tmp_chords = [Chord(int(ch[0]),ch[1]) for ch in sorted(re.findall('chord\(([0-9]+),([ivxmo7]+)\)', ans))]
 				tmp_chords.sort(key=lambda x: x.time)
 				tmp_errors = [Error(int(er[0]),int(er[1])) for er in re.findall('out_error\(([0-9]+),([0-9]+)\)', ans)]
